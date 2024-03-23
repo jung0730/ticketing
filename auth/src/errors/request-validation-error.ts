41d8a56bcf19d91f22297b21c85e312d70abc459
jung0730 <arj2763@gmail.com>
2024-03-23 10:08:20 +0800

    第一版error handling

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,21 @@
+// https://blog.alexanderkaran.com/errors-in-typescript
+
+import { ValidationError } from 'express-validator'
+
+export class RequestValidationError extends Error {
+  statusCode = 400
+  // If you omit public, TypeScript will not automatically create a property for the parameter. 
+  constructor(public errors: ValidationError[]) {
+    super()
+
+    Object.setPrototypeOf(this, RequestValidationError.prototype)
+  }
+  serializeErrors() {
+    return this.errors.map((err) => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: err.msg };
+    });
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/error-handler";
 
 const app = express();
 app.use(json());
@@ -12,6 +13,7 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signupRouter)
 app.use(signoutRouter)
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
